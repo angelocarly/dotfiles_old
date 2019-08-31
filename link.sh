a3b9a6e26bb6ce03164d9d1898345b779e860e3d
angelocarly <angelo.carly@hotmail.com>
2019-08-31 02:33:21 +0200

    Move files to home/

--- link.sh
+++ link.sh
@@ -1,5 +1,7 @@
 wd=$(echo $PWD)
-ln -sT "$wd/config/i3" ~/.config/i3
-ln -sT "$wd/config/polybar" ~/.config/polybar
-ln -sT "$wd/config/wallpapers" ~/.config/wallpapers
-ln -sT "$wd/config/kitty" ~/.config/kitty
+ln -sT "$wd/home/.zshrc" ~/.zshrc
+ln -sT "$wd/home/.config/compton.conf" ~/.config/compton.conf
+ln -sT "$wd/home/.config/i3" ~/.config/i3
+ln -sT "$wd/home/.config/polybar" ~/.config/polybar
+ln -sT "$wd/home/.config/wallpapers" ~/.config/wallpapers
+ln -sT "$wd/home/.config/kitty" ~/.config/kitty
