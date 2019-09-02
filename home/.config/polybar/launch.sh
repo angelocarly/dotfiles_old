314cef97f9295c3d4ad63eebc52b4b1f0c45aeed
angelocarly <angelo.carly@hotmail.com>
2019-09-03 01:35:01 +0200

    move xrandr settings to i3 config

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -6,6 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# Wait until monitors are found for 5 seconds
+echo $(xrandr --query | grep " connected" | cut -d"" -f1) >> /home/magnias/output
+echo $(xrandr --query) >> /home/magnias/output
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload magnias &
