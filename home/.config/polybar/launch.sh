79957ed06682c642aea2c39404a6a5e09bf0f44d
angelocarly <angelo.carly@hotmail.com>
2019-09-02 16:06:14 +0200

    Add polybar to multi-monitor setup

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -6,8 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar magnias 2>&1 &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload magnias &
+  done
+else
+  polybar --reload magnias &
+fi
 
 echo "launched polybar"
 
