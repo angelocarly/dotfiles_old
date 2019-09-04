85f74d8fadf63de939f24806090863bf9b8f1803
angelocarly <angelo.carly@hotmail.com>
2019-09-04 11:58:21 +0200

    Bind workspace keys to monitors

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -5,14 +5,11 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Wait until monitors are found for 5 seconds
-echo $(xrandr --query | grep " connected" | cut -d"" -f1) >> /home/magnias/output
-echo $(xrandr --query) >> /home/magnias/output
+sleep 2
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload magnias &
+    MONITOR=$m polybar --reload magnias & > /magnias/outpp;
   done
 else
   polybar --reload magnias &
