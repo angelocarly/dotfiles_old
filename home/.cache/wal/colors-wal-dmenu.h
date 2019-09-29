4e88cf7bb3f63db38058f6a600df39eb5827345e
angelocarly <angelo.carly@hotmail.com>
2019-09-29 15:44:22 +0200

    Add screenshot

--- home/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c7c7c5", "#211f19" },
-	[SchemeSel] = { "#c7c7c5", "#b1a277" },
-	[SchemeOut] = { "#c7c7c5", "#bc8b9a" },
-};
--- home/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#211f19";
-static const char* selfgcolor   = "#c7c7c5";
-static const char* normbgcolor  = "#b4b27d";
-static const char* normfgcolor  = "#c7c7c5";
-static const char* urgbgcolor   = "#b1a277";
-static const char* urgfgcolor   = "#c7c7c5";
